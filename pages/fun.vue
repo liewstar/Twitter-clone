b2dd5d686df7991aa07e1a28ebd1c070457006c3
liewstar <2935437378@qq.com>
2023-07-09 17:53:36 +0800

    first commit

---
+++ pages/fun.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div>
+            this is fun fun
+        </div>
+    </div>
+</template>
+   
+   <script>
+   export default {
+     name: 'FunPage'
+   }
+   </script>
+   
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">hello world</h1>
+    <h2 class="text-3xl">123</h2>
+    <h3 class="text-8xl underline text-center">123</h3>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
